24cf058740358534a7a57a2f1f3052ccb9df965c
Hom, Kenneth <khom@admins-macbook-air.local>
2015-09-30 13:05:11 -0400

    Initial Commit

---
+++ AmethystApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AmethystApp
+//
+//  Created by Hom, Kenneth on 9/30/15.
+//  Copyright © 2015 Hom, Kenneth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
